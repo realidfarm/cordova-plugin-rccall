8d4e5ec7f0c401373810a92309e54bd1c237b653
taofengno1 <taofeng_ls@sina.com>
2017-10-11 14:33:38 +0800

    添加链接方法

--- www/rccall.js
+++ www/rccall.js
@@ -1,5 +1,8 @@
 var exec = require('cordova/exec');
 
+exports.connect = function(success, error) {
+    exec(success, error, "RCcall", "connect", []);
+};
 exports.voiceCall = function(success, error) {
     exec(success, error, "RCcall", "voiceCall", []);
 };
