946f44832910a1a39c972e8b8dda34a0ec7ac6d3
taofengno1 <taofeng_ls@sina.com>
2017-10-11 14:37:34 +0800

    js链接方法添加token参数

--- www/rccall.js
+++ www/rccall.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
-exports.connect = function(success, error) {
-    exec(success, error, "RCcall", "connect", []);
+exports.connect = function(token, success, error) {
+    exec(success, error, "RCcall", "connect", [token]);
 };
 exports.voiceCall = function(success, error) {
     exec(success, error, "RCcall", "voiceCall", []);
